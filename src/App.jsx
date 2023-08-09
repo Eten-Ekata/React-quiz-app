918630541c15de198fa2823e4fc9e55c02845c05
Eten-Ekata <ekataelishaeten@gmail.com>
2023-08-09 20:44:55 +0100

    component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import Header from './components/Header'
 import './index.css'
+import { useQuiz } from './context/QuizContext'
+import Main from './components/Main'
+import Loader from './components/Loader'
+import Error from './components/Error'
+import StartScreen from './components/StartScreen'
 
 function App() {
+ const {status} = useQuiz()
 
   return (
     <div className='app'>
       <Header/>
+    <Main>
+    {status === "loading" && <Loader />}
+    {status === "error" && <Error />}
+    {status === "ready" && <StartScreen />}
+    </Main>
+
+
+
+
     </div>
   )
 }
